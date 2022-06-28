b1dd523b8e92edba7137dd92e5b3dbb445f52779
DGK <rlaehdrb0923@gmail.com>
2022-06-29 07:03:54 +0900

    feat : swagger setting and feature

--- DRF_simplejwt/urls.py
+++ DRF_simplejwt/urls.py
@@ -1,5 +1,29 @@
-from django.urls import path, include
+from drf_yasg.views             import get_schema_view
+from drf_yasg                   import openapi
+
+from rest_framework.permissions import AllowAny
+
+from django.urls                import path, include, re_path
+
+
+schema_url_patterns = [ 
+    path('users', include('users.urls')),
+]
+
+schema_view_v1 = get_schema_view(
+    openapi.Info(
+        title           = 'DRF-simplejwt',
+        default_version = 'v1',
+        description     = "DRF-simplejwt API",
+    ),
+    public             = True,
+    permission_classes = [AllowAny],
+    patterns           = schema_url_patterns
+)
 
 urlpatterns = [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view_v1.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger$', schema_view_v1.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc$', schema_view_v1.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('users', include('users.urls')),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views       import APIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response    import Response
+from drf_yasg.utils             import swagger_auto_schema
 
 from users.serializers          import SingupSerializers
 
@@ -8,9 +9,19 @@ from users.serializers          import SingupSerializers
 class UserSignupView(APIView):
     permission_classes = [AllowAny]
     
+    @swagger_auto_schema(request_body=SingupSerializers, responses={201: SingupSerializers})
     def post(self, request):
         serializer = SingupSerializers(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
+    
+
+class UserinfoView(APIView):
+    permission_classes = [IsAuthenticated]
+    
+    @swagger_auto_schema(responses={200: 'user authorization success'})
+    def get(self, request):
+        user = request.user
+        return Response({'message' : f'{user.nickname} Authorization success'}, status=200)
