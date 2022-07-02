b3d397ce3812b7c912474f5ebe0fa8ae7ce0dfc1
DGK <rlaehdrb0923@gmail.com>
2022-07-02 13:10:33 +0900

    Add : SignUpSerializer fields

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,14 @@
+from unittest.util import _MAX_LENGTH
+from rest_framework             import serializers
 from rest_framework.serializers import ModelSerializer
 
 from users.models               import User
 
 
 class SignUpSerializer(ModelSerializer):
+    email    = serializers.CharField(max_length=100, required=True)
+    nickname = serializers.CharField(required=False)
+    password = serializers.CharField(required=True, write_only=True)
     
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)  # self.Meta.model.objects.create_user(**validated_data)
