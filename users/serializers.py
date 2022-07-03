10da2b12ccfd5e9d19eefc750b68868002208f75
DGK <rlaehdrb0923@gmail.com>
2022-07-03 12:08:54 +0900

    add : DRF-simplejwt SignIn custom- feature

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,13 @@
 import re
 
-from rest_framework             import serializers
-from rest_framework.serializers import ModelSerializer
+from django.contrib.auth.hashers          import check_password
 
-from users.models               import User
+from rest_framework                       import serializers
+from rest_framework.serializers           import ModelSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.tokens      import OutstandingToken, BlacklistedToken
+
+from users.models import User
 
 
 class SignUpSerializer(ModelSerializer):
@@ -28,3 +32,38 @@ class SignUpSerializer(ModelSerializer):
         model        = User  # get_user_model()
         fields       = ['email', 'nickname', 'password']
         extra_kwargs = {'password' : {'write_only' : True}}
+        
+        
+class SignInSerializer(TokenObtainPairSerializer):
+    email    = serializers.CharField(required=True, max_length=100)
+    password = serializers.CharField(required=True, write_only=True, max_length=100)
+    
+    def validate(self, data):
+        email    = data.get('email')
+        password = data.get('password')
+        
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            raise serializers.ValidationError('detail : user not existed')
+        
+        if not check_password(password, user.password):
+            raise serializers.ValidationError('detail : invalid password')
+
+        for token in OutstandingToken.objects.filter(user=user):
+            BlacklistedToken.objects.get_or_create(token=token)
+        
+        token         = super().get_token(user)
+        refresh_token = str(token)
+        access_token  = str(token.access_token)
+        
+        data = {
+            'refresh' : refresh_token,
+            'access'  : access_token
+        }
+        
+        return data
+        
+    class Meta:
+        model = User
+        fields = ['email', 'password']
--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from users.views import UserSignUpView, UserSignOutView, UserinfoView 
+from users.views import UserSignUpView, UserSignInView, UserSignOutView, UserinfoView 
 
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('/signup', UserSignUpView.as_view()),
-    path('/signin', TokenObtainPairView.as_view()),
+    path('/signin', UserSignInView.as_view()),
     path('/signout', UserSignOutView.as_view()),
     path('/auth', UserinfoView.as_view()),
     path('/token/refresh', TokenRefreshView.as_view()),
