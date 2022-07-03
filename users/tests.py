a4cde9de5f8fcf1bedb60b1d0241c7574a713c2a
DGK <rlaehdrb0923@gmail.com>
2022-07-03 16:11:49 +0900

    test : DRF-simplejwt test code

--- users/tests.py
+++ users/tests.py
@@ -1,11 +1,13 @@
 import json
 
-from rest_framework.test                  import APITestCase
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework.test             import APITestCase
+from rest_framework_simplejwt.tokens import OutstandingToken
 
 from users.models import User
 
 class UserSignUpTest(APITestCase):
+    
+    maxDiff = None
     
     def test_success_user_signup(self):
         data = {
@@ -27,41 +29,28 @@ class UserSignUpTest(APITestCase):
     
 
 class UserSignInTest(APITestCase):
-    def setUp(self):
-        User.objects.create(
-            id = 1,
-            email = 'DGK-test@gmail.com',
+    
+    maxDiff = None
+    
+    @classmethod
+    def setUpTestData(cls):
+        User.objects.create_user(
+            email    = 'DGK-test@gmail.com',
             nickname = 'DGK01',
             password = 'DGK12345678'
         )
-        
-    def tearDown(self):
-        User.objects.all().delete()
 
     def test_success_user_signin(self):
         data = {
-            'email' : 'DGK-test@gmail.com',
+            'email'    : 'DGK-test@gmail.com',
             'nickname' : 'DGK01',
             'password' : 'DGK12345678'
         }
         
-        # print(User.objects.values())
+        response = self.client.post('/users/signin', data=json.dumps(data), content_type='application/json')
+        
         user  = User.objects.get(email='DGK-test@gmail.com')
-        # print(user.id, user.nickname, user.email, user.password)
-        token = TokenObtainPairSerializer.get_token(user)
+        token = OutstandingToken.objects.get(user=user).token
         
-        refresh_token = str(token)
-        access_token  = str(token.access_token)
-        
-        # signup_res = self.client.post('/users/signup', data=json.dumps(data), content_type='application/json')
-        signin_res = self.client.post('/users/signin', data=json.dumps(data), content_type='application/json')
-        
-        
-        # self.assertEqual(response.status_code, 200)
-        self.assertEqual(
-            signin_res.json(),
-            {
-                'refresh' : refresh_token,
-                'access'  : access_token
-            }
-        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(token, response.json()['refresh'])
